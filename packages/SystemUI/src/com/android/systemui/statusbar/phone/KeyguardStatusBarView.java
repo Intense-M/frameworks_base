dfe796bda47d5536f0cb43afde989185c87b8425
beanstown106 <nbenis106@gmail.com>
2015-10-30 11:05:06 -0400

    Show carrier label / custom & change color [1/2] [Squashed]
    *Use statusbar carrier label in the keyguard as well https://github.com/DirtyUnicorns/android_frameworks_base/commit/16cb66de93a57683f0d3efb97da538e93f0bfc2c
    *Fix Carrier label from appearing at random when applying a theme https://github.com/DirtyUnicorns/android_frameworks_base/commit/3297a89dc7774d30df77c35f0a22529e02b7c3cf
    *Fix carrier label not showing up sometimes after a reboot https://github.com/DirtyUnicorns/android_frameworks_base/commit/98585ba020eff62eaccfa12e908d50e1302cda8f
    *That's more refresh https://github.com/DirtyUnicorns/android_frameworks_base/commit/2386187e570ee3087be6aa4bd2b5ddd82e75fa9e
    
    PureNexus edits:
    *Adapt for 5.1
    *Link default color to exposed_kg_carrier_text
    
    SystemUI: Status bar carrier label (1/2)
    * allow choosing to show on keyguard, normal status bar or both
    * fix up some padding issues
    * remove "Emergency Calls Only" message when no sim is installed
    
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    
    Keyguard/Statusbar Carrier label options [1/2]
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    * code originally writted by @Altaf-Mahdi i just squased into one commit
    
    https://github.com/PureNexus/android_frameworks_base_old/commit/16a247ae6468127320c584b053aa095c169d01b8
    
    PureNexus Edits:
    * adapted to replace the carrier label toggle in https://github.com/PureNexus/android_frameworks_base/commit/16a247ae6468127320c584b053aa095c169d01b8
    * this now provides more robust options for carrier label customization
    
    https://github.com/PureNexus/android_frameworks_base_old/commit/688d56fe23f1f7d5c3eedfd78e0ac13d32d1cb84
    
    Carrier label - fix capitilization bug
    * when resetting carrier label to stock it would automatically capitalize it Ex: VERIZON WIRELESS instead of Verizon Wireless, lets change this so when not using a custom carrier label it is exactly like it should be stock
    
    Credit/Thanks to @Altaf-Mahdi @beanstown106 and @martincz
    
    Change-Id: Ia65581b261a67e959d15b084c4ea8f0ef9939f28
    Signed-off-by: vibhu0009 <cvibhu0009@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -18,7 +18,12 @@ package com.android.systemui.statusbar.phone;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -48,11 +53,13 @@ public class KeyguardStatusBarView extends RelativeLayout
     private boolean mKeyguardUserSwitcherShowing;
     private boolean mBatteryListening;
 
-    private TextView mCarrierLabel;
     private View mSystemIconsSuperContainer;
     private MultiUserSwitch mMultiUserSwitch;
     private ImageView mMultiUserAvatar;
     private TextView mBatteryLevel;
+
+    private TextView mCarrierLabel;
+    private int mShowCarrierLabel;
 
     private BatteryController mBatteryController;
     private KeyguardUserSwitcher mKeyguardUserSwitcher;
@@ -60,8 +67,21 @@ public class KeyguardStatusBarView extends RelativeLayout
     private int mSystemIconsSwitcherHiddenExpandedMargin;
     private Interpolator mFastOutSlowInInterpolator;
 
+    private ContentObserver mObserver = new ContentObserver(new Handler()) {
+        public void onChange(boolean selfChange, Uri uri) {
+            showStatusBarCarrier();
+            updateVisibilities();
+        }
+    };
+
     public KeyguardStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        showStatusBarCarrier();
+    }
+
+    private void showStatusBarCarrier() {
+        mShowCarrierLabel = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CARRIER, 1, UserHandle.USER_CURRENT);
     }
 
     @Override
@@ -109,6 +129,16 @@ public class KeyguardStatusBarView extends RelativeLayout
                 mBatteryCharging || showBatteryLevel ? View.VISIBLE : View.GONE);
         boolean showCarrierText = getResources().getBoolean(R.bool.config_showOperatorInKeyguard);
         mCarrierLabel.setVisibility(showCarrierText ? View.VISIBLE : View.GONE);
+
+        if (mCarrierLabel != null) {
+            if (mShowCarrierLabel == 1) {
+                mCarrierLabel.setVisibility(View.VISIBLE);
+            } else if (mShowCarrierLabel == 3) {
+                mCarrierLabel.setVisibility(View.VISIBLE);
+            } else {
+                mCarrierLabel.setVisibility(View.GONE);
+            }
+        }
     }
 
     private void updateSystemIconsLayoutParams() {
@@ -248,5 +278,17 @@ public class KeyguardStatusBarView extends RelativeLayout
     @Override
     public boolean hasOverlappingRendering() {
         return false;
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        getContext().getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                "status_bar_show_carrier"), false, mObserver);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
     }
 }
