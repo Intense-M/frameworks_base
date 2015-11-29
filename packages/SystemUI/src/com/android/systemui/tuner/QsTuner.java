065ae608602bb5f0d77b98287410a65b8b52bcb0
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-11-29 06:34:41 +0000

    Reboot/Recovery QS Tile
    Change-Id: I2c7956b7201e2103420fc7254051141807024fc4
    Signed-off-by: vibhu0009 <cvibhu0009@gmail.com>
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/du_strings.xml

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -207,6 +207,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("screenoff")) return R.string.quick_settings_screen_off;
         else if (spec.equals("brightness")) return R.string.quick_settings_brightness;
         else if (spec.equals("music")) return R.string.quick_settings_music_label;
+        else if (spec.equals("reboot")) return R.string.quick_settings_reboot_label;
         return 0;
     }
 
@@ -457,6 +458,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("screenoff")) return R.drawable.ic_qs_power;
             else if (mSpec.equals("brightness")) return R.drawable.ic_qs_brightness_auto_on_alpha;
             else if (mSpec.equals("music")) return R.drawable.ic_qs_media_play;
+            else if (mSpec.equals("reboot")) return R.drawable.ic_qs_reboot;
             return R.drawable.android;
         }
 
