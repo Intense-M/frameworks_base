28a7c2a2fb8f43bfbc36ee449278ae8291d93ad1
neighbors28 <sjp0728@gmail.com>
2015-12-29 17:48:46 -0500

    Frameworks/base: Second Clock (1/2)
    Dokdo-Project/platform_frameworks_base@6ab5d62#diff-1fa71c270d063a48f921f0760d10c7f3R122
    
    PS2: Fu Whitespace
    PS3: small Issue
    
    Change-Id: Ie04942ae1bb1649d18db51086b46666b56ef1ea0
    Signed-off-by: vibhu0009 <cvibhu0009@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3588,6 +3588,12 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
+         * Display second in the Clock
+         * @hide
+         */
+        public static final String CLOCK_USE_SECOND = "clock_use_second";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
